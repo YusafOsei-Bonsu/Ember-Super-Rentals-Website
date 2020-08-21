2dd8b35d403e4606dd4b927b9d3a3e54d92e8c56
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-08-21 13:48:03 +0100

    Test suite for contact page

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -20,7 +20,7 @@ module('Acceptance | super rentals', function(hooks) {
     assert.equal(currentURL(), "/about", "Reached About page");
   });
 
-  // Test suite for about page
+  // Test suite for Contact page
   test('Visiting /about', async (assert) => {
     // Navigate to and check if the current page is the About page
     await visit('/about');
@@ -31,6 +31,21 @@ module('Acceptance | super rentals', function(hooks) {
 
     await click(".jumbo a.button");
     assert.equal(currentURL(), '/contact', "Reached Contact page");
+  });
+
+  // Test suite for Contact page
+  test('Visiting /contact', async (assert) => {
+    // Navigate to and check if the current page is the About page
+    await visit('/contact');
+    assert.equal(currentURL(), "/contact", "Reached Contact page");
+
+    // Checking if the h2 and About btn element contain the correct text
+    assert.dom("h2").hasText("Contact Us");
+    assert.dom(".jumbo a.button").hasText("About");
+
+    // Check if pressing the About btn navigates to the About page
+    await click(".jumbo a.button");
+    assert.equal(currentURL(), '/about', "Reached About page");
   });
 
 });
