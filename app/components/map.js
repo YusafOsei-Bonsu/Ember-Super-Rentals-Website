e1cf010450133d39782505ec8df309d2f2cda5eb
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-08-22 19:06:05 +0100

    Src method returns request to API

--- app/components/map.js
+++ app/components/map.js
@@ -1,7 +1,19 @@
 import Component from '@glimmer/component';
 import ENV from 'super-rentals/config/environment';
 
+const MAPBOX_API = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static';
+
 class MapComponent extends Component {
+
+    // Returning the request
+    get src() {
+        let { lng, lat, width, height, zoom } = this.args;
+        let coords = `${lng},${lat},${zoom}`;
+        let dim = `${width}x${height}`;
+        let accessToken = `access_token=${this.token}`;
+        return `${MAPBOX_API}/${coords}/${dim}@2x?${accessToken}`;
+    }
+
     get token() {
         return encodeURIComponent(ENV.MAPBOX_ACCESS_TOKEN);
     }
