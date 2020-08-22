ed4aaf8e05b57da05c2e36a6ef4fb5c7104cde2d
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-08-22 12:42:06 +0100

    Test suite for toggling image size

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | rental/image', (hooks) => {
@@ -20,6 +20,32 @@ module('Integration | Component | rental/image', (hooks) => {
     assert.dom(".image").exists();
     assert.dom(".image img").hasAttribute("src", "/assets/images/teaching-tomster.png");
     assert.dom(".image img").hasAttribute("alt", "Teaching Tomster");
-    
   });
+
+  // Testing if pressing the image changes its size
+  test('clicking on the component toggles its size', async (assert) => {
+    await render(hbs`
+      <Rental::Image
+        src="/assets/images/teaching-tomster.png"
+        alt="Teaching Tomster"
+      />
+    `);
+
+    // Checking if the btn surrounding the image exists
+    assert.dom("button.image").exists();
+
+    assert.dom(".image").doesNotHaveClass("large");
+    assert.dom(".image small").hasText("View Larger");
+
+    // Testing the scenario when the image enlarges in size
+    await click("button.image");
+    assert.dom(".image").hasClass("large");
+    assert.dom(".image small").hasText("View Smaller");
+
+    // Testing the scenario when the image reduces in size
+    await click("button.image");
+    assert.dom(".image").doesNotHaveClass("large");
+    assert.dom(".image small").hasText("View Larger");
+  });
+  
 });
