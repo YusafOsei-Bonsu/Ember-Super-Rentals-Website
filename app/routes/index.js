81c6d494da6fdf23c9947783a4c03311f6ae734d
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-08-23 18:47:43 +0100

    Fetching JSON data and displaying it on the index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,20 @@
 import Route from '@ember/routing/route';
 
+const COMMUNITY_CATEGORIES = ["Condo", "Townhouse", "Apartment"]
+
 class IndexRoute extends Route {
     async model() {
-        return {
-            title: "Grand Old Mansion",
-            owner: "Veruca Salt",
-            city: "San Francisco",
-            location: {
-                lat: 37.7749,
-                lng: -122.4194
-            },
-            category: "Estate",
-            type: "Standalone",
-            bedrooms: 15,
-            image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg",
-            description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.'
-        }
+        // Getting the JSON data from the local dummy API
+        let response = await fetch('/api/rentals.json');
+        let { data } = await response.json();
+
+        return data.map(model => {
+            let type, { attributes } = model;
+
+            COMMUNITY_CATEGORIES.includes(attributes.category) ? type = "Community" : type = "Standalone";
+
+            return { type, ...attributes };
+        });
     }
 }
 
